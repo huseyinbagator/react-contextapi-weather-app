ce35b58ebf0764a3c38d610f107d9c0ca1baff93
Hüseyin Bagator <bagatorhuseyin0@gmail.com>
2023-08-21 15:34:33 +0300

    Adding bootstrap. Using a router. Creating the HomePage Component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
-import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import HomePage from "./components/HomePage";
+
+import "./App.css";
 function App() {
   return (
-    <div className="App">
-      <div className="container">
-        <div className="city-name-section">
-          <h1>Aranan Şehir</h1>
-        </div>
-        <div className="search-bar-section">
-          <select className="search-bar"></select>
-        </div>
-        <div className="daily-status-section">
-          <div className="status-container">
-            <div className="status-left"></div>
-            <div className="status-right"></div>
-          </div>
-        </div>
-        <div className="weather-week">
-          <div className="days"></div>
-        </div>
-      </div>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
