9c3128e92dba50d956615c2bbaa22f748ed9b941
Hüseyin Bagator <bagatorhuseyin0@gmail.com>
2023-08-25 11:29:14 +0300

    Employees have been deleted

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -11,9 +11,17 @@ const HomePage = () => {
     precipitation: 0,
     moisture: 20,
     wind: 15,
-    day:"Çarşamba",
-    weather:"Güneşli",
-    weekdays:["Çarşamba","Perşembe","Cuma","Cumartesi","Pazar","Pazartesi","Salı"],
+    day: "Çarşamba",
+    weather: "Güneşli",
+    weekdays: [
+      "Çarşamba",
+      "Perşembe",
+      "Cuma",
+      "Cumartesi",
+      "Pazar",
+      "Pazartesi",
+      "Salı",
+    ],
   };
   return (
     <div className="App d-flex align-items-center">
@@ -26,12 +34,12 @@ const HomePage = () => {
         </Col>
         <Col className="d-flex justify-content-center mt-3">
           <Row className="status-container bg-body d-flex justify-content-center align-items-center">
-            <Col className="status-left m-1 d-flex align-items-center ">
+            <Col className="status-left m-1 d-flex align-items-center">
               <Col className="ms-4 fs-2 text">{data.heat}°C</Col>
-              <Col className="fs-5 ">
+              <Col className="fs-5">
                 <div>Yağış:{data.precipitation}</div>
                 <div>Nem:{data.moisture}</div>
-                <div>Rüzgar:{data.wind}</div>    
+                <div>Rüzgar:{data.wind}</div>
               </Col>
             </Col>
             <Col className="status-right m-1 fs-5">
@@ -43,12 +51,14 @@ const HomePage = () => {
         </Col>
         <Col className="d-flex justify-content-center align-items-center mt-2">
           <div className="days bg-body">
-            <div className="pt-4  fs-3 d-flex justify-content-center align-items-center ">
-             {data.weekdays.map((day,index) =>(
-              <div className="m-3" key={index}>{day}</div>
-             ))}  
+            <div className="pt-4 fs-3 d-flex justify-content-center align-items-center">
+              {data.weekdays.map((day, index) => (
+                <div className="m-3" key={index}>
+                  {day}
+                </div>
+              ))}
             </div>
-         </div>
+          </div>
         </Col>
       </Container>
     </div>
