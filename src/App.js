83a7a35b976a6d054388199655692ca953cf64a4
Hüseyin Bagator <bagatorhuseyin0@gmail.com>
2023-09-03 14:48:35 +0300

    ContextProvider moved to index.js

--- src/App.js
+++ src/App.js
@@ -2,17 +2,14 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import "./App.css";
 import HomePage from "./pages/HomePage";
-import { ContextProvider } from "./contex";
 
 const App = () => {
   return (
-    <ContextProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<HomePage />}></Route>
-        </Routes>
-      </BrowserRouter>
-    </ContextProvider>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 };
 
