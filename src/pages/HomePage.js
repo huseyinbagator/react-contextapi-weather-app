9b1ac532385b8b010502be3659f18b99590e3633
huseyinbagator <bagatorhuseyin0@gmail.com>
2023-09-18 19:42:39 +0300

    displaying data

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -24,7 +24,7 @@ const HomePage = () => {
     <div className="App d-flex align-items-center">
       <Container className="homepage">
         <Col className="text-center mt-4">
-          <h1>{weather.name}</h1>
+          <h1>{weather?.name ? weather.name : "Loading..." }</h1>
         </Col>
         <Col className="mx-3">
           <select className="search-bar rounded-pill"></select>
@@ -32,17 +32,23 @@ const HomePage = () => {
         <Col className="d-flex justify-content-center mt-3">
           <Row className="status-container bg-body d-flex justify-content-center align-items-center">
             <Col className="status-left m-1 d-flex align-items-center">
-              <Col className="ms-4 fs-2 text"></Col>
+              <Col className="ms-4 fs-2 text">
+                {weather?.main?.feels_like ? weather.main.feels_like : "Loading..."}°C
+              </Col>
               <Col className="fs-5">
-                <div></div>
                 <div></div>
                 <div></div>
               </Col>
             </Col>
             <Col className="status-right m-1 fs-5">
-              <h3>Hava Durumu</h3>
-              <div></div>
-              <div></div>
+              <h3>Weather Condition</h3>
+              <div>
+                {weather?.weather && weather.weather.length > 0
+                  ? `${weather.weather[0].main} Description: ${weather.weather[0].description}`
+                  : "Loading..."}
+              </div>
+
+              <div>Wind {weather?.wind?.speed ? weather.wind.speed : "Loading..."}</div>
             </Col>
           </Row>
         </Col>
