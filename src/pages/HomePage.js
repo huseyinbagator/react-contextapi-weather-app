d2030bb773e496f485d0e9180b882b7269a14311
huseyinbagator <bagatorhuseyin0@gmail.com>
2023-09-21 10:07:34 +0300

    displaying data-2

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -25,7 +25,7 @@ const HomePage = () => {
     <div className="App d-flex align-items-center">
       <Container className="homepage">
         <Col className="text-center mt-4">
-          <h1>{weather?.name ? weather.name : "Loading..."}</h1>
+          <h1>{weather?.name || "Loading..."}</h1>
         </Col>
         <Col className="mx-3">
           <select className="search-bar rounded-pill"></select>
@@ -34,9 +34,7 @@ const HomePage = () => {
           <Row className="status-container bg-body d-flex justify-content-center align-items-center">
             <Col className="status-left m-1 d-flex align-items-center">
               <Col className="ms-4 fs-2 text">
-                {weather?.main?.feels_like
-                  ? weather.main.feels_like
-                  : "Loading..."}
+                {weather?.main?.feels_like || "Loading..."}
                 °C
               </Col>
               <Col className="fs-5">
@@ -52,9 +50,7 @@ const HomePage = () => {
                   : "Loading..."}
               </div>
 
-              <div>
-                Wind {weather?.wind?.speed ? weather.wind.speed : "Loading..."}
-              </div>
+              <div>Wind {weather?.wind?.speed || "Loading..."}</div>
             </Col>
           </Row>
         </Col>
