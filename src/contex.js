89ac13f6201b55f9bc97afd5c8b61565c1cbe62b
huseyinbagator <bagatorhuseyin0@gmail.com>
2023-09-26 01:55:32 +0300

    thema-provider

--- src/contex.js
+++ src/contex.js
@@ -4,12 +4,15 @@ export const Context = createContext();
 
 export const ContextProvider = ({ children }) => {
   const [weather, setWeather] = useState({});
+  const [thema, setThema] = useState("light");
 
   return (
     <Context.Provider
       value={{
         weather,
         setWeather,
+        thema,
+        setThema,
       }}
     >
       {children}
